2a5383fa464ff2fe1a20181d114c6ff09df4d01a
MylesMburu <ngicumyles@gmail.com>
2023-12-11 11:50:26 +0300

    added deposit and withdrawal options

--- lib/View/Pages/functions/walalet_operations.dart
+++ lib/View/Pages/functions/walalet_operations.dart
@@ -14,6 +14,17 @@ import '../../themes/colors.dart';
 
 
 class WalletCtrl with ChangeNotifier {
+  String _selectedOption = 'bank';
+  String withdrawalOption = 'own'; // default to 'own'
+
+  String get selectedOption => _selectedOption;
+  set selectedOption(String newValue) {
+    if (_selectedOption != newValue) {
+      _selectedOption = newValue;
+      notifyListeners(); // This will notify all the listening widgets to rebuild
+    }
+  }
+
   WalletCtrl() {
 
   }
@@ -80,118 +91,202 @@ class WalletCtrl with ChangeNotifier {
 
 
 
-  withdrawDialog(BuildContext context) {
-    TextEditingController amountController = TextEditingController();
-    double myWidth = MediaQuery.of(context).size.width;
-    double myHeight = MediaQuery.of(context).size.height;
-    AwesomeDialog(
-      context: context,
-      dialogType: DialogType.NO_HEADER,
-      animType: AnimType.scale,
-      dialogBackgroundColor: AppColors.whiteColor,
-      headerAnimationLoop: false,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text("Withdraw Balance : 80,200", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),),
-            customWalletField(
-              label: 'Enter amount ',
-              require: true,
-              controller: amountController,
-              height: myHeight,
-              width: myHeight,
-              type: TextInputType.number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter amount';
-                }
-                return null;
-              },
-            )
-          ],
-        ),
+  void withdrawDialog(BuildContext context) {
+  TextEditingController amountController = TextEditingController();
+  TextEditingController numberController = TextEditingController(); // Controller for the other number input
+  double myHeight = MediaQuery.of(context).size.height;
+
+  String localWithdrawalOption = withdrawalOption; // Local variable for withdrawal options
+  
+  AwesomeDialog(
+    context: context,
+    dialogType: DialogType.NO_HEADER,
+    animType: AnimType.scale,
+    dialogBackgroundColor: AppColors.whiteColor,
+    headerAnimationLoop: false,
+    body: StatefulBuilder( // Use StatefulBuilder to manage the local state
+      builder: (BuildContext context, StateSetter setState) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To prevent unnecessary space
+            children: [
+              Text(
+                "Withdraw Balance : 80,200",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+              ),
+              // Your customWalletField widget for the amount
+              customWalletField(
+                label: 'Enter amount ',
+                require: true,
+                controller: amountController,
+                height: myHeight,
+                width: myHeight,
+                type: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter amount';
+                  }
+                  return null;
+                },
+              ),
+              RadioListTile<String>(
+                title: const Text('To own number'),
+                value: 'own',
+                groupValue: localWithdrawalOption,
+                onChanged: (value) {
+                  setState(() {
+                    localWithdrawalOption = value!;
+                    withdrawalOption = value; // Update the class-level withdrawal option
+                  });
+                },
+              ),
+              RadioListTile<String>(
+                title: const Text('To other number'),
+                value: 'other',
+                groupValue: localWithdrawalOption,
+                onChanged: (value) {
+                  setState(() {
+                    localWithdrawalOption = value!;
+                    withdrawalOption = value; // Update the class-level withdrawal option
+                  });
+                },
+              ),
+              // Conditionally display the TextField for other number
+              if (localWithdrawalOption == 'other')
+                TextField(
+                  controller: numberController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter other number',
+                    // Add other decoration properties if needed
+                  ),
+                  keyboardType: TextInputType.phone,
+                  // Add other TextField properties if needed
+                ),
+            ],
+          ),
+        );
+      },
+    ),
+    btnCancel: ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).pop(); // Close the dialog
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Colors.red,
       ),
-      btnCancel: ElevatedButton(
-        onPressed: () {
-          // Handle cancel button press
+      child: const Text('Cancel'),
+    ),
+    btnOk: ElevatedButton(
+      onPressed: () {
+        String enteredAmount = amountController.text;
+        // Optional: Validate the other number input if "To other number" is selected
+        if (localWithdrawalOption == 'other' && numberController.text.isEmpty) {
+          // Show an error or handle the validation failure
+        } else {
+          // Use the enteredAmount and withdrawalOption for further processing
+          // If "To other number" is selected, use numberController.text as well
           Navigator.of(context).pop(); // Close the dialog
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.red, // Set the button background color
-        ),
-        child: Text('Cancel'),
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Colors.green,
       ),
-      btnOk: ElevatedButton(
-        onPressed: () {
-          // Handle accept button press with the entered amount
-          String enteredAmount = amountController.text;
-          // You can now use the enteredAmount for further processing
-          Navigator.of(context).pop(); // Close the dialog
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.green, // Set the button background color
-        ),
-        child: Text('Proceed'),
+      child: const Text('Proceed'),
+    ),
+  )..show();
+}
+
+
+
+void depositDialog(BuildContext context) {
+  TextEditingController amountController = TextEditingController();
+  double myWidth = MediaQuery.of(context).size.width;
+  double myHeight = MediaQuery.of(context).size.height;
+  String localSelectedOption = _selectedOption; // Create a local variable for the selected option
+
+  AwesomeDialog(
+    context: context,
+    dialogType: DialogType.NO_HEADER,
+    animType: AnimType.scale,
+    dialogBackgroundColor: AppColors.whiteColor,
+    headerAnimationLoop: false,
+    body: StatefulBuilder( // Use StatefulBuilder to create local state
+      builder: (BuildContext context, StateSetter setState) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                "Deposit Balance : 80,200",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+              ),
+              customWalletField(
+                label: 'Enter amount ',
+                require: true,
+                controller: amountController,
+                height: myHeight,
+                width: myHeight,
+                type: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter amount';
+                  }
+                  return null;
+                },
+              ),
+              RadioListTile<String>(
+                title: const Text('From Bank'),
+                value: 'bank',
+                groupValue: localSelectedOption,
+                onChanged: (value) {
+                  setState(() { // Call the local setState provided by StatefulBuilder
+                    localSelectedOption = value!;
+                    _selectedOption = value; // Update the class-level selected option
+                  });
+                },
+              ),
+              RadioListTile<String>(
+                title: const Text('From Mobile Money (Mpesa)'),
+                value: 'mpesa',
+                groupValue: localSelectedOption,
+                onChanged: (value) {
+                  setState(() { // Call the local setState provided by StatefulBuilder
+                    localSelectedOption = value!;
+                    _selectedOption = value; // Update the class-level selected option
+                  });
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    ),
+    btnCancel: ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).pop(); // Close the dialog
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Colors.red,
       ),
-    )..show();
-  }
-  depositDialog(BuildContext context) {
-    TextEditingController amountController = TextEditingController();
-    double myWidth = MediaQuery.of(context).size.width;
-    double myHeight = MediaQuery.of(context).size.height;
-    AwesomeDialog(
-      context: context,
-      dialogType: DialogType.NO_HEADER,
-      animType: AnimType.scale,
-      dialogBackgroundColor: AppColors.whiteColor,
-      headerAnimationLoop: false,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text("Deposit Balance : 80,200", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),),
-            customWalletField(
-              label: 'Enter amount ',
-              require: true,
-              controller: amountController,
-              height: myHeight,
-              width: myHeight,
-              type: TextInputType.number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter amount';
-                }
-                return null;
-              },
-            )
-          ],
-        ),
+      child: Text('Cancel'),
+    ),
+    btnOk: ElevatedButton(
+      onPressed: () {
+        String enteredAmount = amountController.text;
+        // Handle accept button press with the entered amount and selected option
+        // Use _selectedOption for further processing
+        Navigator.of(context).pop(); // Close the dialog
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Colors.green,
       ),
-      btnCancel: ElevatedButton(
-        onPressed: () {
-          // Handle cancel button press
-          Navigator.of(context).pop(); // Close the dialog
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.red, // Set the button background color
-        ),
-        child: Text('Cancel'),
-      ),
-      btnOk: ElevatedButton(
-        onPressed: () {
-          // Handle accept button press with the entered amount
-          String enteredAmount = amountController.text;
-          // You can now use the enteredAmount for further processing
-          Navigator.of(context).pop(); // Close the dialog
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.green, // Set the button background color
-        ),
-        child: Text('Proceed'),
-      ),
-    )..show();
-  }
+      child: Text('Proceed'),
+    ),
+  )..show();
+}
+
+
   inputBusinessDetails(BuildContext context) {
     TextEditingController businessNameController = TextEditingController();
     TextEditingController amountController = TextEditingController();
